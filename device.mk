a27e5ccb0297900a466a76d6983b4f32d4c66395
kubersharma001 <kubersharma001@gmail.com>
2021-08-15 16:47:58 +0000

    X2: Set surface_flinger default props
    must be set in vendor default.props.
    
    Change-Id: I66e69bd5e5b6cad05979763f4daa1fdb8113a941
    Signed-off-by: Sukrut4778 <sukrut4778@gmail.com>

--- device.mk
+++ device.mk
@@ -160,6 +160,15 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@3.0.vendor \
     vendor.qti.hardware.display.mapper@4.0.vendor
 
+# SF default props
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.has_wide_color_display=true \
+    ro.surface_flinger.has_HDR_display=true \
+    ro.surface_flinger.use_color_management=true \
+    ro.surface_flinger.wcg_composition_dataspace=143261696 \
+    ro.surface_flinger.protected_contents=true \
+    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true
+
 # Doze
 PRODUCT_PACKAGES += \
     devicesettings \
