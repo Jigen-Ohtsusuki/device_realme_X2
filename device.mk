9da567f93d4fccf83db3414b92deefdd8426598e
Daniel Jacob Chittoor <djchittoor47@gmail.com>
2022-10-27 22:05:35 +0530

    X2: Enable R8 optimizations for SystemUI & system_server
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: AnilShebin <anilshebin@gmail.com>

--- device.mk
+++ device.mk
@@ -179,6 +179,10 @@ PRODUCT_PACKAGES += \
     RealmeProximityHelper \
     RealmeDoze
 
+# Enable whole-program R8 Java optimizations for SystemUI and system_server
+SYSTEM_OPTIMIZE_JAVA := true
+SYSTEMUI_OPTIMIZE_JAVA := true
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.x2 \
