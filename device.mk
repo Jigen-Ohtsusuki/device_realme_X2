a2d21296541a56b404776a330c72fa998e74ece5
Raymond2204 <belorkarmayur@gmail.com>
2023-02-15 17:38:53 +0000

    X2: Bring back old devicesettings

--- device.mk
+++ device.mk
@@ -145,7 +145,13 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@1.1.vendor \
     vendor.qti.hardware.display.mapper@2.0.vendor \
     vendor.qti.hardware.display.mapper@3.0.vendor \
-    vendor.qti.hardware.display.mapper@4.0.vendor \
+    vendor.qti.hardware.display.mapper@4.0.vendor
+
+# Doze
+PRODUCT_PACKAGES += \
+    devicesettings \
+    RealmeProximityHelper \
+    RealmeDoze
 
 # Fingerprint
 PRODUCT_PACKAGES += \
