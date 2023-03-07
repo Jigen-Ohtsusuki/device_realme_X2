11cf285d6e208ad9c11018d5d5c17a0905612447
Retypolkg <retypolkg2021@gmail.com>
2023-03-07 09:15:44 +0100

    X2: Initial AOSPA bringup

--- device.mk
+++ device.mk
@@ -18,7 +18,7 @@ PRODUCT_PRODUCT_VNDK_VERSION := current
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-pa
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage/lineage-sdk
