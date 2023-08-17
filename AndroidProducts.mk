0089af16de850e5d1344f44c172d6100157784ab
Raymond2204 <mayurbelorkar8@gmail.com>
2023-08-17 17:48:02 +0000

    X2: RisingOSS BringUp

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,12 @@
 #
-# Copyright (C) 2022 The droidxOS Project
+# Copyright (C) 2022 The lineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/droidx_X2.mk
+    $(LOCAL_DIR)/lineage_X2.mk
 
 COMMON_LUNCH_CHOICES := \
-    droidx_X2-userdebug \
-    droidx_X2-eng
+    lineage_X2-userdebug \
+    lineage_X2-eng
--- lineage_X2.mk
+++ lineage_X2.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022 The droidxOS Project
+# Copyright (C) 2022 The lineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -9,14 +9,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common droidx stuff.
-$(call inherit-product, vendor/droidx/config/common_full_phone.mk)
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from X2 device
 $(call inherit-product, device/realme/X2/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := droidx_X2
+PRODUCT_NAME := lineage_X2
 PRODUCT_DEVICE := X2
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := realme X2
@@ -32,10 +32,18 @@ TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # UDFPS Animations
-EXTRA_UDFPS_ANIMATIONS := true
+#EXTRA_UDFPS_ANIMATIONS := true
 
-# DroidX UI
-DROIDX_GAPPS := true
+# Inherit some RisingOSS stuff
+RISING_CHIPSET := "Snapdragon 730G"
+RISING_MAINTAINER := "Levi Ackerman"
+TARGET_BUILD_APERTURE_CAMERA := true
+TARGET_HAS_UDFPS := true
+TARGET_USE_PIXEL_FINGERPRINT := true
+WITH_GMS := true
+TARGET_CORE_GMS := true
+TARGET_USE_GOOGLE_TELEPHONY := false
+TARGET_CORE_GMS_EXTRAS := true
 
 BUILD_FINGERPRINT := "google/cheetah/cheetah:13/TQ3A.230705.001/10216780:user/release-keys"
 
