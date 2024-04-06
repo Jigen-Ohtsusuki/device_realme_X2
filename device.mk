ff6bd39af54271e6304dfc6fc2b97d41728dc49b
AceBlade007x <acebladelevi007x@gmail.com>
2024-04-06 01:24:14 +0000

    X2: Switch back to QTI PowerHAL
    This reverts commit 025eb9866e3ec657d0ec8c4794f06d2d98b19b65.

--- device.mk
+++ device.mk
@@ -389,10 +389,7 @@ PPRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.3.vendor \
-    android.hardware.power-service.x2-libperfmgr \
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/power-libperfmgr/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+    android.hardware.power-service-qti
 
 # QMI
 PRODUCT_PACKAGES += \
@@ -447,9 +444,7 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    vendor/nxp/opensource/sn100x \
-    hardware/google/interfaces \
-    hardware/google/pixel
+    vendor/nxp/opensource/sn100x
 
 # Improve scrolling
 PRODUCT_PROPERTY_OVERRIDES += \
